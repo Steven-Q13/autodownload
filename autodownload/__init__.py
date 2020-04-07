98c44d28d1696fc4dee823bbbde70e41d659a8aa
Steven-Q13 <sequeen0@gmail.com>
2020-04-06 23:14:38 -0400

    When intiliazed it moves launchd file

--- autodownload/__init__.py
+++ autodownload/__init__.py
@@ -1,45 +1,29 @@
-import os.path, sys, getpass, shutil
+import os, getpass, subprocess, sys
 
-from cryptography.fernet import Fernet
+def main():
+    path = os.path.realpath(__file__)
+    path = path.split('/')
+    path.pop(-1)
+    path.pop(-1)
+    path = '/'.join(path)
 
-class Setup:
-	
-	def __init__(password, json_path) {
-		user = getpass.getuser()
-	
-		with open('/Users/%d/.auto_download/auto_download_info.txt' % user, 'w') as save:
-			key = fernet.generate_key();
-			with open('/Users/%d/.auto_download/auto_download_token.txt' % user, 'wb') as token:
-				pickle.dump(key, token)
-			f = fernet(key)
-			password_encrypted.f.encrypt(password)
-			save.write(password_encrypted)
-			
-		shutil.move(json_path, '/Users/%d/.auto_download/credentials.json' % user)	
-		
-		#Look for file storing the credential object
-		if os.path.exists('/Users/%d/.auto_download/token.pickle' % user):
-			with open('/Users/%d/.auto_download/token.pickle' % user) as token:
-				creds = pickle.load(token)
-	
-		#If file didn't exist or credentials aren't valid reauthenticate
-		# Opens pop-up window to authenticate
-		# Uses json generated from getting API ID	
-		if not creds or not creds.valid:
-			if creds and creds.expired and creds.refresh_token:
-				creds.refresh(Request())
-			else:
-				flow = InstalledAppFlow.from_client_secrets_file('/Users/%d/.auto_download/credentials.json' % user, SCOPES)
-				creds = flow.run_local_server(port=0)
-		
-			#Saves newly gotten credentials
-			with open('/Users/%d/.auto_download/token.pickle' % user, 'wb') as token:
-				pickle.dump(creds, token)
+    with open('%s/launchd/com.autodownload.plist' % path, 'r') as f:
+        contents = f.read()
+    
+    contents = contents.replace('JJJ1', '%s/bin/checkmail.py' % path)
+    contents = contents.replace('JJJ2', '%s/launchd/stoud.log' % path)
+    contents = contents.replace('JJJ3', '%s/launchd/sterr.log' % path)
+    contents = contents.replace('JJJ4', path)
+    
+    user = getpass.getuser()
+    launchd_path = '/Users/%s/Library/LaunchAgents/com.autodownload.plist' % user
+    
+    with open(launchd_path, 'w') as fnew:
+        fnew.write(contents)
+
+    subprocess.call(['launchctl', 'load', launchd_path])
 
 
-		shutil.move('./com.auto_download.plist', '/Users/%d/Library/LaunchAgents/com.auto_download.plist' % user)
-		shutil.move('./check_email', '/usr/local/bin')
-	
-	}
 
-	def main():
+main()
+
